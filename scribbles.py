1b90c40c7dfb9182957248d0d6ef7be2a46f69de
Julia <52479551+JuliaChernykh@users.noreply.github.com>
2021-05-28 13:41:35 +0300

    Tests added

---
+++ scribbles.py
@@ -0,0 +1,56 @@
+def generate_rect(corner, h, w):
+    rect = []
+    for i in range(h):
+        for j in range(w):
+            rect.append((corner[0]+i, corner[1]+j))
+    return rect
+
+
+def generate_interval(a, b, fat=1):
+    x0, y0 = a[0], a[1]
+    x1, y1 = b[0], b[1]
+    interval = []
+    deltax = abs(x1 - x0)
+    deltay = abs(y1 - y0)
+    error = 0
+    deltaerr = (deltay + 1)
+    y = y0
+    diry = y1 - y0
+    if diry > 0:
+        diry = 1
+    if diry < 0:
+        diry = -1
+    for x in range(x0, x1+1):
+        for j in range(fat):
+            interval.append((x, y+j))
+        error = error + deltaerr
+        if error >= (deltax + 1):
+            y = y + diry
+            error = error - (deltax + 1)
+    return interval
+
+
+def generate_scribbles_0_3():
+    obj = generate_interval((130, 50), (165, 200), 5)
+    obj += generate_rect((110, 260), 30, 4)
+    bkg = generate_rect((50, 10), 3, 115)
+    return obj, bkg
+
+
+def generate_scribbles_4():
+    obj = generate_rect((40, 55), 60, 3)
+    obj += generate_interval((98, 160), (131, 212), 3)
+    bkg = generate_rect((14, 47), 4, 69)
+    bkg += generate_rect((240, 78), 4, 69)
+    return obj, bkg
+
+def generate_scribbles_5():
+    obj = generate_rect((40, 55), 60, 3)
+    obj += generate_interval((98, 160), (131, 212), 3)
+    bkg = generate_rect((14, 47), 4, 69)
+    bkg += generate_rect((240, 78), 4, 69)
+    return obj, bkg
+
+
+scribbles = [generate_scribbles_0_3, generate_scribbles_0_3, generate_scribbles_0_3, generate_scribbles_0_3,
+             generate_scribbles_4, generate_scribbles_5]
--- segmentation-tests/test.py
+++ segmentation-tests/test.py
@@ -6,65 +6,14 @@ import matplotlib.image as mpimg
 from Dinic import Graph
 from PIL import Image
 from image2graph import image2graph
+from scribbles import scribbles
 import os
-
-
-def generate_rect(corner, h, w):
-    rect = []
-    for i in range(h):
-        for j in range(w):
-            rect.append((corner[0]+i, corner[1]+j))
-    return rect
-
-
-def generate_interval(a, b, fat=1):
-    x0, y0 = a[0], a[1]
-    x1, y1 = b[0], b[1]
-    interval = []
-    deltax = abs(x1 - x0)
-    deltay = abs(y1 - y0)
-    error = 0
-    deltaerr = (deltay + 1)
-    y = y0
-    diry = y1 - y0
-    if diry > 0:
-        diry = 1
-    if diry < 0:
-        diry = -1
-    for x in range(x0, x1+1):
-        for j in range(fat):
-            interval.append((x, y+j))
-        error = error + deltaerr
-        if error >= (deltax + 1):
-            y = y + diry
-            error = error - (deltax + 1)
-    return interval
-
-
-def generate_scribbles_0_3():
-    obj = generate_interval((130, 50), (165, 200), 5)
-    obj += generate_rect((110, 260), 30, 4)
-    bkg = generate_rect((50, 10), 3, 115)
-    return obj, bkg
-
-
-def generate_scribbles_4_4():
-    obj = generate_interval((130, 50), (165, 200), 5)
-    obj += generate_rect((110, 260), 30, 4)
-    bkg = generate_rect((50, 10), 3, 115)
-    return obj, bkg
 
 
 def make_pixels(index, image_url):
     img = mpimg.imread('../images-320/' + image_url)
     img_shape = img.shape
-    obj_scribble, bkg_scribble = [], []
-    if 0 <= index <= 3:
-        obj_scribble, bkg_scribble = generate_scribbles_0_3()
-    elif index:
-        obj_scribble, bkg_scribble = generate_scribbles_4_4()
-    else:
-        obj_scribble, bkg_scribble = generate_scribbles_4_4()
+    obj_scribble, bkg_scribble = scribbles[index]()
     underlying_graph = image2graph(img, obj_scribble, bkg_scribble)
     start = time.time()
     flow = underlying_graph.dinic()
@@ -80,6 +29,21 @@ def make_pixels(index, image_url):
         if val:
             bin_img.putpixel((i % img_shape[1], i // img_shape[1]), 1)
     bin_img.save('../images-segments-prediction-320/' + image_url, 'JPEG')
+
+
+def solo_test(index):
+    directory = '../images-320'
+    images = os.listdir(directory)
+    images.sort()
+    image_url = images[index]
+    make_pixels(index, image_url)
+    true_segments = mpimg.imread('../image-segments-320/' + image_url.split('-')[0] + '-320.jpg')
+    predicted_segments = mpimg.imread('../images-segments-prediction-320/' + image_url)
+    true_segments = true_segments[:, :, :1].flatten().reshape(predicted_segments.shape)
+    correct_pixels, Jaccard = analyze(true_segments, predicted_segments)
+    print('Правильно угаданных пикселей: ', correct_pixels)
+    print('Мера Жаккара: ', Jaccard)
+    print('--------------------------------------------------------')
 
 
 def analyze(true_segments, predicted_segments):
@@ -115,4 +79,5 @@ def analyze_all():
         print('--------------------------------------------------------')
 
 
-analyze_all()
+# analyze_all()
+solo_test(5)
